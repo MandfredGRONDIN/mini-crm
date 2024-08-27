84f00a947305a4f15abddaa3ee50c3c1a4fbce12
Hiren Patel <12210923+hirenpatelfr@users.noreply.github.com>
2024-08-27 12:17:00 +0200

    fixed basic test with jest configuration

--- front/src/jest.config.js
+++ front/src/jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+    moduleNameMapper: {
+        '\\.(css|less)$': 'identity-obj-proxy'
+    }
 };
